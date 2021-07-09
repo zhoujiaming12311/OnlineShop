d1e509ab791d8572ef13a8a35552a716405c4fe0
zhoujiaming <230103@qq.com>
2021-07-09 13:53:31 +0800

    20210709 1353

--- src/components/goods/List.vue
+++ src/components/goods/List.vue
@@ -19,6 +19,20 @@
                     <el-button type="primary">添加商品</el-button>
                 </el-col>
             </el-row>
+
+            <!-- table表格区域 -->
+            <el-table :data="goodsList" border stripe>
+                <el-table-column type="index"></el-table-column>
+                <el-table-column label="商品名称" prop="goods_name"></el-table-column>
+                <el-table-column label="商品价格(元)" prop="goods_price" width="95px"></el-table-column>
+                <el-table-column label="商品重量" prop="goods_weight" width="70px"></el-table-column>
+                <el-table-column label="操作" width="140px">
+                    <template>
+                         <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+                         <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
         </el-card>
     </div>
 </template>
@@ -34,9 +48,9 @@
                     pagesize: 10
                 },
                 // 商品列表
-                goodslist: [],
+                goodsList: [],
                 // 总数据条数
-                total: 55
+                total: 0
             }
         },
         created() {
@@ -47,11 +61,12 @@
             async getGoodsList() {
                 //   const { data: res } = await this.$http.get('goods', { params: this.queryInfo })
                 var res = {
-                    goods: [{ goods_id: 926, cat_id: null, goods_name: '海外购-鞋子' }, { goods_id: 927, cat_id: null, goods_name: '海外购-鞋子2' }, { goods_id: 928, cat_id: null, goods_name: '海外购-鞋子3' }, { goods_id: 929, cat_id: null, goods_name: '海外购-鞋子4' }],
+                    goods: [{ goods_id: 926, cat_id: null, goods_name: '海外购-鞋子1', goods_price: 100, goods_weight: 20 }, { goods_id: 927, cat_id: null, goods_name: '海外购-鞋子2', goods_price: 120, goods_weight: 30 }, { goods_id: 928, cat_id: null, goods_name: '海外购-鞋子3', goods_price: 130, goods_weight: 40 }, { goods_id: 929, cat_id: null, goods_name: '海外购-鞋子4', goods_price: 140, goods_weight: 50 }],
                     pagenum: '1',
-                    total: 916
+                    total: 3
                 }
-                this.goodslist = res
+                this.goodsList = res.goods
+                this.total = res.goods.total
                 this.$message.success('获取商品列表成功！')
             }
         }
